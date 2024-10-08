8c773e79c29905c03d39d2d05f1cdcc50890a0f1
yusufabdo20 <yusufa.fattah@gmail.com>
2024-10-08 22:08:09 +0300

    sorting imports .

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,3 +1,7 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 import 'package:doctor_flutter_v1/config/routes/app_page.dart';
 import 'package:doctor_flutter_v1/config/routes/app_router.dart';
 import 'package:doctor_flutter_v1/config/theme.dart';
@@ -5,9 +9,6 @@ import 'package:doctor_flutter_v1/core/services/cache/app_cache_key.dart';
 import 'package:doctor_flutter_v1/core/services/cache/cache_service.dart';
 import 'package:doctor_flutter_v1/model/otp_model.dart';
 import 'package:doctor_flutter_v1/presentation/auth/otp/view/otp_view.dart';
-import 'package:easy_localization/easy_localization.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
     if (context.locale == const Locale("ar")) {
       CacheService.setData(key: AppCacheKey.lang, value: "ar");
     }
-    
+
     return ScreenUtilInit(
       designSize: const Size(360, 690),
       minTextAdapt: true,
