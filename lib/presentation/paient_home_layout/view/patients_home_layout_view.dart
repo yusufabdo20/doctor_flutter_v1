877e5cccc240f300043d60fd69c6b72e6a750983
yusufabdo20 <yusufa.fattah@gmail.com>
2024-10-09 15:18:29 +0300

    Refactor patient home layout view to remove unused imports
    Remove unused imports from the `patients_home_layout_view.dart` file to improve code clarity and reduce clutter. Specifically, the following imports were removed:
    - `doctor_home_layout_cubit.dart`
    - `app_color.dart`
    - `app_style.dart`
    - `custom_text.dart`
    - `doctor_home_layout_data.dart`
    - `custom_app_bar.dart`
    - `custom_bottom_nav_bar.dart`
    
    This change ensures that only necessary imports are included, making the file easier to maintain and understand.

--- lib/presentation/paient_home_layout/view/patients_home_layout_view.dart
+++ lib/presentation/paient_home_layout/view/patients_home_layout_view.dart
@@ -1,11 +1,5 @@
-import 'package:doctor_flutter_v1/controller/doctor_home_layout/doctor_home_layout_cubit.dart';
 import 'package:doctor_flutter_v1/controller/patient_home_layout/patient_home_layout_cubit.dart';
-import 'package:doctor_flutter_v1/core/utils/app_color.dart';
-import 'package:doctor_flutter_v1/core/utils/app_style.dart';
-import 'package:doctor_flutter_v1/core/widgets/custom_text.dart';
-import 'package:doctor_flutter_v1/presentation/doctor_home_layout/data/doctor_home_layout_data.dart';
-import 'package:doctor_flutter_v1/presentation/doctor_home_layout/widgets/custom_app_bar.dart';
-import 'package:doctor_flutter_v1/presentation/doctor_home_layout/widgets/custom_bottom_nav_bar.dart';
+
 import 'package:doctor_flutter_v1/presentation/paient_home_layout/widgets/custom_app_bar.dart';
 import 'package:doctor_flutter_v1/presentation/paient_home_layout/widgets/custom_bottom_nav_bar.dart';
 import 'package:flutter/material.dart';
