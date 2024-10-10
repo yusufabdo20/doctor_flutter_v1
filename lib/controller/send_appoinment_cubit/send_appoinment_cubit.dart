419a86fb7324b5ca0283cf29fcc5766879eee987
yusufabdo20 <yusufa.fattah@gmail.com>
2024-10-11 01:15:09 +0300

    AppoinmentRepo

--- lib/controller/send_appoinment_cubit/send_appoinment_cubit.dart
+++ lib/controller/send_appoinment_cubit/send_appoinment_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
 
 import '../../model/appoinment_model.dart';
 
@@ -7,4 +8,9 @@ part 'send_appoinment_state.dart';
 
 class SendAppoinmentCubit extends Cubit<SendAppoinmentState> {
   SendAppoinmentCubit() : super(SendAppoinmentInitial());
+  final TextEditingController bloodPressureController = TextEditingController();
+  final TextEditingController temperatureController = TextEditingController();
+  final TextEditingController heartRateController = TextEditingController();
+  final TextEditingController treatmentPlanController = TextEditingController();
+  sendAppoinment() {}
 }
---
+++ lib/repo/appoinment_repo.dart
@@ -0,0 +1,43 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+
+import '../core/error/error_hander.dart';
+import '../core/network/dio_helper.dart';
+import '../core/network/end_point.dart';
+import '../core/services/cache/app_cache_key.dart';
+import '../core/services/cache/cache_service.dart';
+import '../model/appoinment_model.dart';
+
+abstract class AppoinmentRepo {
+  Future<Either<Failures, AppoinmentModel>> getAppoinment({
+    required String bloodPressure,
+    required num temperature,
+    required num heartRate,
+    required String treatmentPlan,
+  });
+}
+
+class AppoinmentRepoImp extends AppoinmentRepo {
+  @override
+  Future<Either<Failures, AppoinmentModel>> getAppoinment({
+    required String bloodPressure,
+    required num temperature,
+    required num heartRate,
+    required String treatmentPlan,
+  }) async {
+    try {
+      Response response = await DioHelper.postData(
+          url: EndPoint.submitHealthRecords,
+          data: {
+            "blood_pressure": bloodPressure,
+            "heart_rate": heartRate,
+            "temperature": temperature,
+            "treatment_plan": treatmentPlan
+          },
+          token: CacheService.getString(key: AppCacheKey.token));
+      return Right(AppoinmentModel.fromJson(response.data));
+    } on DioException catch (e) {
+      return Left(ServerFailure.fromDioError(e));
+    }
+  }
+}
