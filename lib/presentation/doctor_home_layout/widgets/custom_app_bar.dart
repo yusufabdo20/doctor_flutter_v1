1873a54afb29504aa171b8f6e97b3188fd5e048a
amratef503092 <amr.atef503092@gmail.com>
2024-10-19 20:36:10 +0300

    add

--- lib/presentation/doctor_home_layout/widgets/custom_app_bar.dart
+++ lib/presentation/doctor_home_layout/widgets/custom_app_bar.dart
@@ -3,9 +3,13 @@ import 'package:doctor_flutter_v1/core/utils/app_color.dart';
 import 'package:doctor_flutter_v1/core/utils/app_style.dart';
 import 'package:doctor_flutter_v1/core/widgets/custom_text.dart';
 import 'package:doctor_flutter_v1/presentation/doctor_home_layout/data/doctor_home_layout_data.dart';
+import 'package:doctor_flutter_v1/presentation/message_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../core/widgets/localization_button.dart';
+import '../../../notification_module/notification_module/notification_screen.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   const CustomAppBar({
@@ -23,8 +27,21 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
             style: AppStyle.textStyle20BoldKufram,
             color: AppColor.white,
           ),
-          actions: DoctorHomeLayoutData.appBarActions(
-              context)[DoctorHomeLayoutCubit.get(context).selectedIndex],
+          actions: [
+            IconButton(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const NotificationScreen()));
+                },
+                icon: const Icon(Icons.notifications)),
+            LocalizationButton(),
+            IconButton(
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const MessageScreen()));
+                },
+                icon: const Icon(Icons.message)),
+          ],
         );
       },
     );
